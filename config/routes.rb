63e2a1b9ee39543621c70a301006008ebd7c271e
mharribey <mathieuharribey@hotmail.fr>
2016-12-18 16:14:05 +0100

    update style and redirect root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
+      as :user do
+        get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+        put 'users' => 'devise/registrations#update', :as => 'user_registration'
+      end
 
   resources :posts
-  
-  root 'home#index'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
