ae3bd73ab7cea1e33c7ff827c0f682cbd41da195
Roxane Dulhoste <ROXANE@MacBook-Air-de-Roxane.local>
2017-01-05 17:21:33 +0100

    update style

--- db/migrate/20161217154334_create_posts.rb
+++ db/migrate/20161217154334_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.string :title
       t.string :description
-      t.string :image, placeholder: "Placez l'url de l'image"
+      t.string :image
 
       t.timestamps
     end
