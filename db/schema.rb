d8fab16a4fa22dff5b33a8c250a5433b7a92159f
Bastien Bahuet <bastienbahuet@MacBook-Pro-de-Bastien.local>
2017-01-11 11:47:53 +0100

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106105213) do
-
-
+ActiveRecord::Schema.define(version: 20170111095254) do
+  
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,11 +31,9 @@ ActiveRecord::Schema.define(version: 20170106105213) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "image"
     t.string   "video"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
