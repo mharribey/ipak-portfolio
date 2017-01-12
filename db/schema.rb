a1ace287fa7ae3567ad3da4da96792d4105d6043
Roxane Dulhoste <ROXANE@MacBook-Air-de-Roxane.local>
2017-01-12 10:18:16 +0100

    fuck yeah

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,8 @@ ActiveRecord::Schema.define(version: 20170111095254) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "description"
+    t.string   "description"
+    t.string   "image"
     t.string   "video"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
